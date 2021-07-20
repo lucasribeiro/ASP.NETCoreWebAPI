1ee7c61807c7f735ec5a0f05620a27fd8791505d
Lucas Luis Ribeiro <lucas.luisribeiro@gmail.com>
2021-07-19 21:32:59 -0300

    TalkToApi Comit Inicial

---
+++ TalkToApi/Repository/TalkToApiContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TalkToApi.V1.Models;
+
+namespace TalkToApi.Repository
+{
+    public class TalkToApiContext : IdentityDbContext<ApplicationUser>
+    {
+        public TalkToApiContext(DbContextOptions<TalkToApiContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Message> Message { get; set; }
+    }
+}
---
+++ TalkToApi/V1/Models/Message.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TalkToApi.V1.Models
+{
+    public class Message
+    {
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey("FromId")]
+        public ApplicationUser From { get; set; }
+        [ForeignKey("ToId")]
+        public ApplicationUser To { get; set; }
+        public string FromId { get; set; }
+        public string ToId { get; set; }
+        public string Text { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+
+    }
+}
